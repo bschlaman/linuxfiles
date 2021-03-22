8d3d940fd481bbb44505f4ebb0a972b56d0f6c2d
Brendan Schlaman <brendan.schlaman@gmail.com>
2021-03-22 18:56:21 -0400

    making aliases file

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -52,11 +52,11 @@ select opt in $options ; do
 done
 
 # other programs
+echo 'export PS1="\[\e[1m\]\[\e[34m\][\[\e[33m\]\u\[\e[32m\]@\[\e[31m\]\h \[\e[35m\]\W\$(prompt_git \"\[\e[37m\] on \[\e[32m\]\")\[\e[34m\]]\[\e[37m\]$ \[\e[0m\]"' >> ~/.aliases
 PROGRAMS="vim jq htop"
 echo -e "You might want to install..."
 echo -e "sudo pacman -Sy ${PROGRAMS}"
 
 echo -e "${BLD}source ~/.bashrc${NC}"
-echo 'export PS1="\[\e[1m\]\[\e[34m\][\[\e[33m\]\u\[\e[32m\]@\[\e[31m\]\h \[\e[35m\]\W\$(prompt_git \"\[\e[37m\] on \[\e[32m\]\")\[\e[34m\]]\[\e[37m\]$ \[\e[0m\]"'
 
 popd > /dev/null

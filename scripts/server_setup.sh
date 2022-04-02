346adaa2596b2b499e5c92797861bcfa1436abee
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-04-02 18:34:26 -0400

    adding which to programs

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -3,18 +3,18 @@
 # on an archlinux instance so that bootstrap.sh can be run
 
 essential_programs="glibc vim git"
-other_programs="jq rsync htop man cronie docker python-pip ipython"
+other_programs="jq rsync htop man cronie docker python-pip ipython which"
 hosts="newgoog newgoog2 newserver"
 
 for host in $hosts; do
-	echo -- Setting up mirrorlist on $host
-	ssh $host 'echo -e "## United States\nServer = https://mirrors.mit.edu/archlinux/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist'
-	echo -- Installing archlinux-keyring on $host
-	ssh $host sudo pacman --noconfirm -Sy archlinux-keyring
-	echo -- Installing $(echo $essential_programs $other_programs | sed 's/ /, /g') on $host
-	ssh $host sudo pacman --noconfirm -Sy $essential_programs $other_programs
-	echo -- Cloning linuxfiles on $host
-	ssh $host git clone https://github.com/bschlaman/linuxfiles.git
+	# echo -- Setting up mirrorlist on $host
+	# ssh $host 'echo -e "## United States\nServer = https://mirrors.mit.edu/archlinux/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist'
+	# echo -- Installing archlinux-keyring on $host
+	# ssh $host sudo pacman --noconfirm -Sy archlinux-keyring
+	# echo -- Installing $(echo $essential_programs $other_programs | sed 's/ /, /g') on $host
+	# ssh $host sudo pacman --noconfirm -Sy $essential_programs $other_programs
+	# echo -- Cloning linuxfiles on $host
+	# ssh $host git clone https://github.com/bschlaman/linuxfiles.git
 	echo -- Generating ssh keypair on $host
 	ssh $host '< /dev/zero ssh-keygen -q -N ""'
 done

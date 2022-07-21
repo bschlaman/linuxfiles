6df0f447d4fc8c26646e7a9413c3461713e051b7
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-07-21 22:32:24 +0000

    add vim-javascript

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -6,3 +6,19 @@ GRN="\033[0;32m"
 BLU="\033[0;96m"
 BLD="\033[0;1m"
 NC="\033[0;0;39m"
+
+function red(){
+	echo -e ${RED}$@${NC}
+}
+function yel(){
+	echo -e ${YEL}$@${NC}
+}
+function grn(){
+	echo -e ${GRN}$@${NC}
+}
+function blu(){
+	echo -e ${BLU}$@${NC}
+}
+function bld(){
+	echo -e ${BLD}$@${NC}
+}

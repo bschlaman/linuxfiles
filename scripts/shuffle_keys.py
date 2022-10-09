e038a1104d626f3ad3fcb7323c25c67dd7f1a853
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-10-08 20:43:10 -0700

    add shuffle_keys

---
+++ scripts/shuffle_keys.py
@@ -0,0 +1,18 @@
+import random
+
+keys = [
+    "A",
+    "D",
+    "G",
+    "C",
+    "F",
+    "B♭",
+    "E♭",
+    "A♭",
+    "D♭",
+    "G♭",
+    "B",
+    "E",
+]
+random.shuffle(keys)
+print(keys)

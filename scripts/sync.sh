7147d66f36ac4b635082ba0317b0942f5602d169
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-11-29 13:12:52 -0500

    add some helpful info to rsync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,4 +4,12 @@ SERVER=$1
 USER=$(whoami)
 LOCAL_DIR=$2
 REMOTE_DIR=$3
-rsync -rzcltp --progress --delete $LOCAL_DIR $USER@$SERVER:$REMOTE_DIR
+rsync -rzcltpN --progress --delete $LOCAL_DIR $USER@$SERVER:$REMOTE_DIR
+
+# rsync -ac --progress $LOCAL_DIR $USER@$SERVER:$REMOTE_DIR
+
+# -c skip based on cksum, not mod-time & size
+# -l copy symlinks as symlinks
+# -p preserve permissions
+# -t preserve timestamps
+# -N preserve create times (newness)

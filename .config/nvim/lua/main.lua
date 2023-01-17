dfc7acc98dcbca1d87aa00a077edb40ed6a2e5d9
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-01-16 23:32:49 -0800

    diagnostics on hover

--- .config/nvim/lua/main.lua
+++ .config/nvim/lua/main.lua
@@ -1,1 +1,27 @@
-require'lspconfig'.pyright.setup{}
+
+vim.opt.mouse = nil
+
+vim.o.updatetime = 250
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+local on_attach = function(client, bufnr)
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = 'rounded',
+				source = 'always',
+				prefix = ' ',
+				-- want to trigger if cursor is anywhere on the line
+				-- scope = 'cursor',
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end
+	})
+end
+
+require'lspconfig'.pyright.setup{
+	on_attach = on_attach,
+}

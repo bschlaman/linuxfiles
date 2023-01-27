090d10b8fb73087da9902ca667ed3f99591f6540
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-01-27 05:17:20 +0000

    add type hint on T

--- .config/nvim/lua/main.lua
+++ .config/nvim/lua/main.lua
@@ -5,6 +5,7 @@ vim.o.updatetime = 250
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 local on_attach = function(client, bufnr)
+	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 	vim.api.nvim_create_autocmd("CursorHold", {
 		buffer = bufnr,
 		callback = function()
@@ -20,6 +21,9 @@ local on_attach = function(client, bufnr)
 			vim.diagnostic.open_float(nil, opts)
 		end
 	})
+
+	local opts = { noremap=true, silent=true }
+	buf_set_keymap('n', 'T', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 end
 
 require'lspconfig'.pyright.setup{

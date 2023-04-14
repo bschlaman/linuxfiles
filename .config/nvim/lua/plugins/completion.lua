5842c3c283c80485799ead732ae156481336d166
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-04-14 23:13:46 +0000

    trying out new completion

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,12 +1,20 @@
 local cmp = require("cmp")
 
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
 cmp.setup({
 	sources = cmp.config.sources({
-		{ name = "nvim_lua" },
-		{ name = "nvim_lsp" },
+		-- { name = "nvim_lua" },
+		-- { name = "nvim_lsp" },
 		{ name = "path" },
-		{ name = "cmdline" },
-		{ name = "buffer", keyword_length = 5 },
+		-- { name = "cmdline" },
+		-- { name = "emoji" },
+		-- { name = "tmux", option = { all_panes = false } },
+		-- { name = "buffer", keyword_length = 4, option = {
+		-- 	get_bufnrs = function()
+		-- 		return vim.api.nvim_list_bufs()
+		-- 	end
+		-- }},
 	}),
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -18,4 +26,24 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		format = require("lspkind").cmp_format({
+			menu = ({
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[LuaSnip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[Latex]",
+			})
+		}),
+	},
+})
+
+-- Use buffer source for `/` and `?`
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	}
 })
--- .vimrc
+++ .vimrc
@@ -253,9 +253,13 @@ call plug#begin()
 	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 	Plug 'williamboman/mason.nvim'
 	Plug 'williamboman/mason-lspconfig.nvim'
+	Plug 'onsails/lspkind.nvim'
 
 	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'hrsh7th/cmp-nvim-lua'
+	Plug 'andersevenrud/cmp-tmux'
 	Plug 'hrsh7th/cmp-buffer'
+	Plug 'hrsh7th/cmp-emoji'
 	Plug 'hrsh7th/cmp-path'
 	Plug 'hrsh7th/cmp-cmdline'
 	Plug 'hrsh7th/nvim-cmp'

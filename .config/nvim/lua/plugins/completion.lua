eb71a925e6a714f9803fcbde5023b493cfed85fa
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-04-17 07:54:52 -0700

    fix nvim cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -4,17 +4,16 @@ vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
 cmp.setup({
 	sources = cmp.config.sources({
-		-- { name = "nvim_lua" },
-		-- { name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
 		{ name = "path" },
-		-- { name = "cmdline" },
-		-- { name = "emoji" },
-		-- { name = "tmux", option = { all_panes = false } },
-		-- { name = "buffer", keyword_length = 4, option = {
-		-- 	get_bufnrs = function()
-		-- 		return vim.api.nvim_list_bufs()
-		-- 	end
-		-- }},
+		{ name = "emoji" },
+		{ name = "tmux", option = { all_panes = false } },
+		{ name = "buffer", keyword_length = 4, option = {
+			get_bufnrs = function()
+				return vim.api.nvim_list_bufs()
+			end
+		}},
 	}),
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -30,20 +29,37 @@ cmp.setup({
 		fields = { "abbr", "kind", "menu" },
 		format = require("lspkind").cmp_format({
 			menu = ({
-				buffer = "[Buffer]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[LuaSnip]",
 				nvim_lua = "[Lua]",
-				latex_symbols = "[Latex]",
+				nvim_lsp = "[LSP]",
+				path = "[path]",
+				cmdline = "[cmdline]",
+				emoji = "[emoji]",
+				tmux = "[tmux]",
+				buffer = "[Buffer]",
+				-- luasnip = "[LuaSnip]",
+				-- latex_symbols = "[Latex]",
 			})
 		}),
 	},
 })
 
--- Use buffer source for `/` and `?`
 cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = "buffer" },
 	}
+})
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" }
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = { "Man", "!" }
+			}
+		}
+	})
 })

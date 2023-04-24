4232c9d88ae7d9003bfc687f9f66a399c8372466
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-04-24 07:55:01 -0700

    telescope improvements

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+local actions_state = require('telescope.actions.state')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
@@ -8,14 +11,23 @@ vim.keymap.set('n', '<leader>gg', function()
 	builtin.grep_string({ search = vim.fn.input("grep > ") });
 end)
 
+local function close_buffer(prompt_bufnr)
+	local curr_picker = actions_state.get_current_picker(prompt_bufnr)
+	local entry = actions_state.get_selected_entry()
+	vim.cmd('bdelete ' .. entry.bufnr)
+	curr_picker:delete_selection(function(selection) return selection.bufnr == entry.bufnr end)
+end
+
 require('telescope').setup{
 	defaults = {
 		mappings = {
 			i = {
-				["<C-s>"] = require('telescope.actions').select_horizontal
+				["<C-s>"] = actions.select_horizontal,
+				["<C-d>"] = close_buffer,
 			},
 			n = {
-				["<C-s>"] = require('telescope.actions').select_horizontal
+				["<C-s>"] = actions.select_horizontal,
+				["<C-d>"] = close_buffer,
 			},
 		},
 	},
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
 	ensure_installed = {
 		"vim",
-		"help",
+		"vimdoc",
 		"lua",
 		"query",
 		"bash",

162ab5c5ba699a6e77a96a2a67fb7f3bc6fd7a4e
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-09-06 23:18:15 -0700

    workaround for vscode nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -25,7 +25,9 @@ require("nvim-treesitter.configs").setup {
 	},
 	auto_install = false,
 	highlight = {
-		enable = true,
+		-- workaround for vscode-neovim highlighting getting weird
+		-- when treesitter highlight is enabled
+		enable = vim.g.vscode and false or true,
 		additional_vim_regex_highlighting = false,
 	},
 }

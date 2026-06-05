011e8ba336cb4c515bcf0129224da80f419e5ae6
Brendan Schlaman <brendan.schlaman@gmail.com>
2026-06-05 19:40:02 +0000

    remove unused

--- .config/nvim/lua/main.lua
+++ .config/nvim/lua/main.lua
@@ -5,9 +5,4 @@ if vim.g.vscode then
 	vim.cmd("syntax off")
 	vim.g.clipboard = vim.g.vscode_clipboard
 	vim.o.cmdheight = 100 -- required for neovim-0.11+
-else
-	-- vscode-neovim recommends not overriding these
-	-- 0: globally
-	-- (Is this even working?  "Identifier" doesn't seem to match anything, at least for python)
-	-- vim.api.nvim_set_hl(0, "Identifier", { fg = "LightGray" })
 end
